bb77fdd15fc7378304c2713cdfc0038553aeea67
Chaitanya <chaitanya.maili@gmail.com>
2020-03-22 16:13:16 +0530

    Initial start to build webservice

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/chaitanyamaili/go-webservice/models"
+)
 
 func main() {
-	fmt.Println("Hello Go inside webservice!")
+	u := models.User{
+		ID:        1,
+		FirstName: "Chaitanya",
+		LastName:  "Maili",
+	}
+	fmt.Println(u)
 }
