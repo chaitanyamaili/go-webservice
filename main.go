a5ea6306abf50926c21200f38a181a2ff551c745
Chaitanya <chaitanya.maili@gmail.com>
2020-03-22 15:08:25 +0530

    Done thw basic primitive and complex data types.

--- main.go
+++ main.go
@@ -3,5 +3,52 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Go inside webservice!")
+	var arr [3]int
+	arr[0] = 1
+	arr[1] = 2
+	arr[2] = 3
+	fmt.Println(arr)
+
+	arr2 := [3]int{1, 2, 3}
+	fmt.Println(arr2)
+
+	slice := arr2[:]
+	fmt.Println(arr2, slice)
+
+	// slice without defined length.
+	slice2 := []int{1, 2, 3}
+	fmt.Println(slice2)
+
+	slice2 = append(slice2, 4)
+	slice2 = append(slice2, 42, 47)
+	slice2 = append(slice2, 12, 45)
+	fmt.Println(slice2)
+
+	// Working with maps.
+	m := map[string]int{"foo": 12, "bar": 34, "a": 0}
+	fmt.Println(m)
+	// Update the value in map.
+	m["foo"] = 123
+	fmt.Println(m)
+	// Delete the key from the map.
+	delete(m, "foo")
+	fmt.Println(m)
+
+	// Working with struct data type.
+	type user struct {
+		ID        int
+		firstname string
+		lastname  string
+	}
+	var u user
+	u.ID = 1
+	u.firstname = "First"
+	u.lastname = "Last"
+	fmt.Println(u)
+
+	u2 := user{ID: 1,
+		firstname: "ram",
+		lastname:  "govind",
+	}
+	fmt.Println(u2)
 }
